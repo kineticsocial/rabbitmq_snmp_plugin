48511b3754fc3cfc489955ce5ed5607743ef10da
Scott Brooks <scott.brooks@epicadvertising.com>
2010-02-11 09:58:07 -0500

    4 spaces rather then tabs

--- src/rabbit_snmp.erl
+++ src/rabbit_snmp.erl
@@ -5,30 +5,30 @@
 -include("RABBITMQ-MIB.hrl").
 
 load_snmp_environment() ->
-	Path = code:lib_dir(rabbitmq,snmp),
+    Path = code:lib_dir(rabbitmq,snmp),
 
-	ok = application:load(snmp),
-	{ok, Agent} = application:get_env(snmp, agent),
-	Config = proplists:get_value(config, Agent),
-	NewConfig = lists:keystore(dir, 1, Config, {dir, Path ++ "/agent/conf"}),
-	NewAgent = lists:keystore(db_dir, 1, (lists:keystore(config, 1, Agent, {config, NewConfig})), {db_dir, Path ++ "/agent/db"}),
-	application:set_env(snmp, agent, NewAgent),
+    ok = application:load(snmp),
+    {ok, Agent} = application:get_env(snmp, agent),
+    Config = proplists:get_value(config, Agent),
+    NewConfig = lists:keystore(dir, 1, Config, {dir, Path ++ "/agent/conf"}),
+    NewAgent = lists:keystore(db_dir, 1, (lists:keystore(config, 1, Agent, {config, NewConfig})), {db_dir, Path ++ "/agent/db"}),
+    application:set_env(snmp, agent, NewAgent),
 
-	ok = application:start(snmp),
-	ok = snmpa:load_mibs(snmp_master_agent, [Path ++ "/RABBITMQ-MIB"]),
-	ok.
+    ok = application:start(snmp),
+    ok = snmpa:load_mibs(snmp_master_agent, [Path ++ "/RABBITMQ-MIB"]),
+    ok.
 
 start() ->
-	load_snmp_environment(),
-	rabbit_snmp_sup:start_link(),
-	ok.
+    load_snmp_environment(),
+    rabbit_snmp_sup:start_link(),
+    ok.
 
 stop() ->
-	ok.
+    ok.
 
 start(normal, []) ->
-	load_snmp_environment(),
-	rabbit_snmp_sup:start_link().
+    load_snmp_environment(),
+    rabbit_snmp_sup:start_link().
 
 stop(_State) ->
-	ok.
+    ok.
--- src/rabbit_snmp_sup.erl
+++ src/rabbit_snmp_sup.erl
@@ -4,14 +4,14 @@
 -export([start_link/0, init/1]).
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, _Arg = []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, _Arg = []).
 
 init([]) ->
-	{ok, {{one_for_one, 3, 10},
-			[{rabbit_snmp_worker,
-					{rabbit_snmp_worker, start_link, []},
-					permanent,
-					10000,
-					worker,
-					[rabbit_snmp_worker]}
-			]}}.
+    {ok, {{one_for_one, 3, 10},
+            [{rabbit_snmp_worker,
+                    {rabbit_snmp_worker, start_link, []},
+                    permanent,
+                    10000,
+                    worker,
+                    [rabbit_snmp_worker]}
+            ]}}.
